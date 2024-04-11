caf41c5baace878fd0be7cf17db28be0a3345541
Brendan McCall <brendanmccall3@gmail.com>
2024-04-11 10:17:10 -0600

    Using map to register staffs

--- src/main/kotlin/net/brendanmccall/beximitesmagic/item/ModItemGroups.kt
+++ src/main/kotlin/net/brendanmccall/beximitesmagic/item/ModItemGroups.kt
@@ -1,7 +1,13 @@
 package net.brendanmccall.beximitesmagic.item
 
 import net.brendanmccall.beximitesmagic.BeximitesMagic
+import net.brendanmccall.beximitesmagic.item.ModItems.registerItem
+import net.brendanmccall.beximitesmagic.item.ModItems.staffColors
+import net.brendanmccall.beximitesmagic.item.ModItems.staffItems
+import net.brendanmccall.beximitesmagic.item.ModItems.staffTypes
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
@@ -20,61 +26,12 @@ object ModItemGroups {
         Identifier(BeximitesMagic.mod_id, "beximitesmagic"),
         FabricItemGroup.builder()
             .displayName(Text.translatable("itemgroup.beximitesmagic"))
-            .icon { ItemStack(ModItems.STAFF_WOOD) }
+            .icon { ItemStack(ModItems.staffItems["staff_wood"]) }
             .entries { _, entries ->
                 // Adding items to creative mode group
-                entries.add(ModItems.STAFF_WOOD)
-                entries.add(ModItems.STAFF_WOOD_WHITE);
-                entries.add(ModItems.STAFF_WOOD_BLUE);
-                entries.add(ModItems.STAFF_WOOD_TURQUOISE);
-                entries.add(ModItems.STAFF_WOOD_GREEN);
-                entries.add(ModItems.STAFF_WOOD_RED);
-                entries.add(ModItems.STAFF_WOOD_YELLOW);
-                entries.add(ModItems.STAFF_WOOD_GOLD);
-                entries.add(ModItems.STAFF_WOOD_ORANGE);
-                entries.add(ModItems.STAFF_WOOD_GRAY);
-                entries.add(ModItems.STAFF_WOOD_PURPLE);
-                entries.add(ModItems.STAFF_WOOD_PINK);
-                entries.add(ModItems.STAFF_WOOD_BLACK);
-                entries.add(ModItems.STAFF_IRON)
-                entries.add(ModItems.STAFF_IRON_WHITE);
-                entries.add(ModItems.STAFF_IRON_BLUE);
-                entries.add(ModItems.STAFF_IRON_TURQUOISE);
-                entries.add(ModItems.STAFF_IRON_GREEN);
-                entries.add(ModItems.STAFF_IRON_RED);
-                entries.add(ModItems.STAFF_IRON_YELLOW);
-                entries.add(ModItems.STAFF_IRON_GOLD);
-                entries.add(ModItems.STAFF_IRON_ORANGE);
-                entries.add(ModItems.STAFF_IRON_GRAY);
-                entries.add(ModItems.STAFF_IRON_PURPLE);
-                entries.add(ModItems.STAFF_IRON_PINK);
-                entries.add(ModItems.STAFF_IRON_BLACK);
-                entries.add(ModItems.STAFF_DIAMOND)
-                entries.add(ModItems.STAFF_DIAMOND_WHITE);
-                entries.add(ModItems.STAFF_DIAMOND_BLUE);
-                entries.add(ModItems.STAFF_DIAMOND_TURQUOISE);
-                entries.add(ModItems.STAFF_DIAMOND_GREEN);
-                entries.add(ModItems.STAFF_DIAMOND_RED);
-                entries.add(ModItems.STAFF_DIAMOND_YELLOW);
-                entries.add(ModItems.STAFF_DIAMOND_GOLD);
-                entries.add(ModItems.STAFF_DIAMOND_ORANGE);
-                entries.add(ModItems.STAFF_DIAMOND_GRAY);
-                entries.add(ModItems.STAFF_DIAMOND_PURPLE);
-                entries.add(ModItems.STAFF_DIAMOND_PINK);
-                entries.add(ModItems.STAFF_DIAMOND_BLACK);
-                entries.add(ModItems.STAFF_NETHERITE)
-                entries.add(ModItems.STAFF_NETHERITE_WHITE);
-                entries.add(ModItems.STAFF_NETHERITE_BLUE);
-                entries.add(ModItems.STAFF_NETHERITE_TURQUOISE);
-                entries.add(ModItems.STAFF_NETHERITE_GREEN);
-                entries.add(ModItems.STAFF_NETHERITE_RED);
-                entries.add(ModItems.STAFF_NETHERITE_YELLOW);
-                entries.add(ModItems.STAFF_NETHERITE_GOLD);
-                entries.add(ModItems.STAFF_NETHERITE_ORANGE);
-                entries.add(ModItems.STAFF_NETHERITE_GRAY);
-                entries.add(ModItems.STAFF_NETHERITE_PURPLE);
-                entries.add(ModItems.STAFF_NETHERITE_PINK);
-                entries.add(ModItems.STAFF_NETHERITE_BLACK);
+                staffItems.values.forEach { item ->
+                    entries.add(item)
+                }
 
             }
             .build()
--- src/main/kotlin/net/brendanmccall/beximitesmagic/item/ModItems.kt
+++ src/main/kotlin/net/brendanmccall/beximitesmagic/item/ModItems.kt
@@ -11,14 +11,30 @@ object ModItems {
 
     fun registerModItems() {
         BeximitesMagic.logger.info("Registering Mod Items for ${BeximitesMagic.mod_id}");
+        registerStaffItems();
     }
 
     fun registerItem(name: String, item: Item): Item {
         return Registry.register(Registries.ITEM, Identifier(BeximitesMagic.mod_id, name), item);
     }
 
+    // Initialize map for staffs
+    public val staffTypes = listOf("wood", "iron", "diamond", "netherite")
+    public val staffColors = listOf("", "white", "blue", "turquoise", "green", "red", "yellow", "gold", "orange", "gray", "purple", "pink", "black")
+    public val staffItems: MutableMap<String, Item> = mutableMapOf()
+
     // Registering Items
-    val STAFF_WOOD: Item = registerItem("staff_wood", Item(FabricItemSettings().maxCount(1)));
+    fun registerStaffItems()
+    {
+        staffTypes.forEach() { staffType ->
+            staffColors.forEach { color ->
+                val name = "staff_${staffType}${if (color.isNotEmpty()) "_$color" else ""}"
+                staffItems[name] = registerItem(name, Item(FabricItemSettings().maxCount(1)))
+            }
+        }
+    }
+
+    /*val STAFF_WOOD: Item = registerItem("staff_wood", Item(FabricItemSettings().maxCount(1)));
     val STAFF_WOOD_WHITE: Item = registerItem("staff_wood_white", Item(FabricItemSettings().maxCount(1)));
     val STAFF_WOOD_BLUE: Item = registerItem("staff_wood_blue", Item(FabricItemSettings().maxCount(1)));
     val STAFF_WOOD_TURQUOISE: Item = registerItem("staff_wood_turquoise", Item(FabricItemSettings().maxCount(1)));
@@ -69,5 +85,5 @@ object ModItems {
     val STAFF_NETHERITE_GRAY: Item = registerItem("staff_netherite_gray", Item(FabricItemSettings().maxCount(1)));
     val STAFF_NETHERITE_PURPLE: Item = registerItem("staff_netherite_purple", Item(FabricItemSettings().maxCount(1)));
     val STAFF_NETHERITE_PINK: Item = registerItem("staff_netherite_pink", Item(FabricItemSettings().maxCount(1)));
-    val STAFF_NETHERITE_BLACK: Item = registerItem("staff_netherite_black", Item(FabricItemSettings().maxCount(1)));
+    val STAFF_NETHERITE_BLACK: Item = registerItem("staff_netherite_black", Item(FabricItemSettings().maxCount(1)));*/
 }
